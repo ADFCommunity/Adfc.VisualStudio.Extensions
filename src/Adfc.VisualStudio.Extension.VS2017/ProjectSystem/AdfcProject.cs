26d241e9e62cfe199aed34fdf1fe2f5e9493c2b1
Andy Zivkovic <zivkovic.andy@gmail.com>
2017-09-20 07:42:52 +0200

    Create minimal project type registration for .adfcproj files

---
+++ src/Adfc.VisualStudio.Extension.VS2017/ProjectSystem/AdfcProject.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.VS;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.ComponentModel.Composition;
+
+namespace Adfc.VisualStudio.Extension.VS2017.ProjectSystem
+{
+    [Export]
+    [AppliesTo(UniqueCapability)]
+    [ProjectTypeRegistration(ProjectGuid,
+      "ADF Community Project",
+      "Data Factory (*.adfcproj);*.adfcproj",
+      "adfcproj",
+      "Data Factory",
+      PossibleProjectExtensions = "adfcproj")]
+    internal class AdfcProject
+    {
+        internal const string UniqueCapability = "AdfcProject";
+
+        internal const string ProjectGuid = "1cb5cf53-4394-4cd2-9deb-9cb709f77ed2";
+
+        [ImportingConstructor]
+        public AdfcProject(UnconfiguredProject unconfiguredProject)
+        {
+            ProjectHierarchies = new OrderPrecedenceImportCollection<IVsHierarchy>(projectCapabilityCheckProvider: unconfiguredProject);
+        }
+
+        [Import]
+        internal UnconfiguredProject UnconfiguredProject { get; private set; }
+
+        [Import]
+        internal IActiveConfiguredProjectSubscriptionService SubscriptionService { get; private set; }
+
+        [Import]
+        internal IProjectThreadingService ProjectThreadingService { get; private set; }
+
+        [Import]
+        internal ActiveConfiguredProject<ConfiguredProject> ActiveConfiguredProject { get; private set; }
+
+        [Import]
+        internal ActiveConfiguredProject<AdfcProjectBuildConfiguration> MyActiveConfiguredProject { get; private set; }
+
+        [ImportMany(ExportContractNames.VsTypes.IVsProject, typeof(IVsProject))]
+        internal OrderPrecedenceImportCollection<IVsHierarchy> ProjectHierarchies { get; private set; }
+
+        internal IVsHierarchy ProjectHierarchy
+        {
+            get { return this.ProjectHierarchies.Single().Value; }
+        }
+    }
+}
---
+++ src/Adfc.VisualStudio.Extension.VS2017/ProjectSystem/ProjectProperties.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.ProjectSystem;
+using Microsoft.VisualStudio.ProjectSystem.Properties;
+using System.ComponentModel.Composition;
+
+namespace Adfc.VisualStudio.Extension.VS2017.ProjectSystem
+{
+    internal class ProjectProperties : StronglyTypedPropertyAccess
+    {
+        [ImportingConstructor]
+        protected ProjectProperties(ConfiguredProject configuredProject)
+            : base(configuredProject)
+        {
+        }
+
+        protected ProjectProperties(ConfiguredProject configuredProject, IProjectPropertiesContext projectPropertiesContext)
+            : base(configuredProject, projectPropertiesContext)
+        {
+        }
+
+        protected ProjectProperties(ConfiguredProject configuredProject, UnconfiguredProject unconfiguredProject)
+            : base(configuredProject, unconfiguredProject)
+        {
+        }
+
+        protected ProjectProperties(ConfiguredProject configuredProject, string file, string itemType, string itemName)
+            : base(configuredProject, file, itemType, itemName)
+        {
+        }
+    }
+}
